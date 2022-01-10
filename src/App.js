9d957fee681caed7e1c04858024e1b59c0cca5f9
Sankhya <79346892+sayachua1@users.noreply.github.com>
2022-01-10 20:58:38 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Timeline from './compenents/Timeline';
+
+
+const App = () => (
+  <>
+    <h1>React Timeline</h1>
+    <Timeline />
+  </>
+);
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,57 @@
+const data = [
+    {
+        text: 'Wrote my first blog post ever on Medium',
+        date: 'December 23 2016',
+        category: {
+            tag: 'blog',
+            color: 'red'
+        },
+        link: {
+            url:
+            'https://create-react-app.dev/docs/getting-started#yarn',
+            text: 'Read More'
+        }
+    },
+    {
+        text: 'Wrote my first blog post ever on Medium',
+        date: 'December 23 2016',
+        category: {
+            tag: 'app-ideas',
+            color: 'yellow'
+        },
+        link: {
+            url:
+            'https://create-react-app.dev/docs/getting-started#yarn',
+            text: 'Read More'
+        }
+    },
+    {
+        text: 'Wrote my first blog post ever on Medium',
+        date: 'December 23 2016',
+        category: {
+            tag: 'medium',
+            color: '#018169'
+        },
+        link: {
+            url:
+            'https://create-react-app.dev/docs/getting-started#yarn',
+            text: 'Read More'
+        }
+    },
+    {
+        text: 'Wrote my second blog post ever on Medium',
+        date: 'December 23 2016',
+        category: {
+            tag: 'medium',
+            color: '#018169'
+        },
+        link: {
+            url:
+            'https://create-react-app.dev/docs/getting-started#yarn',
+            text: 'Read More'
+        }
+    }
+
+];
+
+export default data;
